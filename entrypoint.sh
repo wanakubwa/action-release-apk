25e7471a7abe0ea03277d41c26c10d87e14bc008
Fabian Berda <wanakubwa@gmail.com>
2020-11-18 00:21:20 +0100

    Update checkout from entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-hub checkout master
+hub checkout main
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/app-release/*-release.apk -m "Build# ${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
